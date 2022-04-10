d80283c09294945a6fa6a6aec40407b5b38bcf78
Desislav Gradev <desislav.gradev@gmail.com>
2022-04-10 20:12:08 +0300

    adding some lines

--- src/ATM.java
+++ src/ATM.java
@@ -6,23 +6,47 @@ public class ATM {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, User> numberToUsers = new HashMap<>();
+        //Filling the map with data
         String[] initial = scanner.nextLine().split(":");
         for (String s : initial) {
             String[] userInfo = s.split("_");
             String name = userInfo[0];
             String cardNumber = userInfo[1];
             String password = userInfo[2];
-            double ballance = Double.parseDouble(userInfo[3]);
-            User user = new User(name, cardNumber, password, ballance);
+            double balance = Double.parseDouble(userInfo[3]);
+            User user = new User(name, cardNumber, password, balance);
             numberToUsers.put(cardNumber, user);
         }
 
-        String[] userInfo = initial[0].split("_");
         System.out.println("Въведете номер на карта или 0 за изход:");
         String input = scanner.nextLine();
         while (!input.equals("0")) {
-            if (numberToUsers.containsKey(input)) {
-                //TODO
+            User currentUser = numberToUsers.get(input);
+            if (currentUser != null) {
+                String name = currentUser.getName();
+                String password = currentUser.getPassword();
+                double balance = currentUser.getBalance();
+                System.out.println("Въведете парола:");
+                String currentPass = scanner.nextLine();
+                if (currentPass.equals(password)) {
+                    System.out.printf("Здравейте, %s%n", name);
+                    System.out.println("Изберете операция:");
+                    System.out.println("(1) Теглене");
+                    System.out.println("(2) Внасяне");
+                    System.out.println("(3) Настройки");
+                    String operation = scanner.nextLine();
+                    switch (operation) {
+                        case "1":
+
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+
+                    }
+
+                }
             } else {
                 System.out.println("Вашата карта е невалидна!");
                 System.out.println("Моля, опитайте отново.");
@@ -35,13 +59,29 @@ public class ATM {
         String name;
         String cardNumber;
         String password;
-        double ballance;
+        double balance;
 
-        User(String name, String cardNumber, String password, double ballance) {
+        User(String name, String cardNumber, String password, double balance) {
             this.name = name;
             this.cardNumber = cardNumber;
             this.password = password;
-            this.ballance = ballance;
+            this.balance = balance;
+        }
+
+        String getName() {
+            return this.name;
+        }
+
+        String getCardNumber() {
+            return this.cardNumber;
+        }
+
+        String getPassword() {
+            return this.password;
+        }
+
+        double getBalance() {
+            return this.balance;
         }
     }
 }
